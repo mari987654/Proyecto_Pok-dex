af95ad5a258f36d37de8910a6547fed0f9a4216b
mari987654 <marilyn28pardo@gmail.com>
2025-03-25 20:32:57 -0500

    agregar introduccion

--- src/componentes/Inicio.tsx
+++ src/componentes/Inicio.tsx
@@ -7,6 +7,7 @@ export function Inicio() {
       <div className="inicio-content">
         <h1>Bienvenido a la Pokédex</h1>
         <p>Explora el maravilloso mundo de los Pokémon</p>
+        <p>En tus manos ahora reposa el último modelo de Pokédex, una maravilla tecnológica diseñada para entrenadores que buscan ir más allá de lo conocido.</p>
         <Link to="/pokemones" className="empezar-btn">
           Empezar Pokédex
         </Link>
